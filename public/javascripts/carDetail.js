bad580679d0d983e16f067b6a187bfafc0866933
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-05-03 19:06:41 -0400

    car detail page picture navigation working

--- public/javascripts/carDetail.js
+++ public/javascripts/carDetail.js
@@ -4,38 +4,44 @@ const sideImgs = document.querySelectorAll(".side-imgs img");
 const controlNext = document.querySelector(".next-control img");
 const controlPrevious = document.querySelector(".previous-control img");
 
-firstSideImg.classList.add("selected-thumbnail");
+firstSideImg.classList.add("selected");
 
 sideImgs.forEach((s) => {
   s.addEventListener("click", (s) => {
-    sideImgs.forEach((i) => i.classList.remove("selected-thumbnail"));
-    s.target.classList.add("selected-thumbnail");
+    sideImgs.forEach((i) => i.classList.remove("selected"));
+    s.target.classList.add("selected");
     mainImg.setAttribute("src", `${s.target.src}`);
   });
 });
 
 controlNext.addEventListener("click", () => {
   for (let i = 0; i < sideImgs.length; i++) {
-    if (sideImgs[i].classList.contains("selected-thumbnail")) {
-      sideImgs[i].classList.remove("selected-thumbnail");
+    if (sideImgs[i].classList.contains("selected")) {
+      sideImgs[i].classList.remove("selected");
       if (i == sideImgs.length - 1) {
-        sideImgs[0].classList.add("selected-thumbnail");
+        sideImgs[0].classList.add("selected");
+        mainImg.setAttribute("src", `${sideImgs[0].src}`);
       } else {
-        sideImgs[(i += 1)].classList.add("selected-thumbnail");
+        sideImgs[(i += 1)].classList.add("selected");
+        mainImg.setAttribute("src", `${sideImgs[i].src}`);
       }
+      return;
     }
   }
 });
 
 controlPrevious.addEventListener("click", () => {
   for (let i = 0; i < sideImgs.length; i++) {
-    if (sideImgs[i].classList.contains("selected-thumbnail")) {
-      sideImgs[i].classList.remove("selected-thumbnail");
-    } /*
-    if (i == 0) {
-      sideImgs[sideImgs.length - 1].classList.add("selected-thumbnail");
-    } else {
-      sideImgs[(i -= 1)].classList.add("selected-thumbnail");
-    }*/
+    if (sideImgs[i].classList.contains("selected")) {
+      sideImgs[i].classList.remove("selected");
+      if (i == 0) {
+        mainImg.setAttribute("src", `${sideImgs[sideImgs.length - 1].src}`);
+        sideImgs[sideImgs.length - 1].classList.add("selected");
+      } else {
+        sideImgs[(i -= 1)].classList.add("selected");
+        mainImg.setAttribute("src", `${sideImgs[i].src}`);
+      }
+      return;
+    }
   }
 });
