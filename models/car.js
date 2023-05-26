c80914361aae75ede3a75b2818b7251727e31ce5
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-05-25 20:00:28 -0400

    Added mongoose timestamps for Car model

--- models/car.js
+++ models/car.js
@@ -2,57 +2,60 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const CarSchema = new Schema({
-  makeName: { type: String, required: true },
-  make: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "Make",
-  },
-  modelName: { type: String, required: true },
-  model: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "Model",
-  },
-  modelVariant: { type: String },
-  year: { type: Number, required: true },
-  price: { type: Number, required: true },
-  mileage: { type: Number, required: true },
-  status: {
-    type: String,
-    required: true,
-    enum: ["Available", "Sold"],
-    default: "Available",
-  },
-  color: {
-    type: String,
-    required: true,
-    enum: [
-      "Black",
-      "Blue",
-      "Brown",
-      "Grey",
-      "Green",
-      "Red",
-      "Silver",
-      "White",
-      "Yellow",
-      "Other",
-    ],
-  },
-  version: { type: Schema.Types.ObjectId, required: true, ref: "Version" },
-  pictures: [
-    {
-      type: Buffer,
-      ref: "Pic",
+const CarSchema = new Schema(
+  {
+    makeName: { type: String, required: true },
+    make: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Make",
     },
-  ],
-  description: { type: String },
-  country: { type: String },
-  email: { type: String },
-  phone: { type: String },
-});
+    modelName: { type: String, required: true },
+    model: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "Model",
+    },
+    modelVariant: { type: String },
+    year: { type: Number, required: true },
+    price: { type: Number, required: true },
+    mileage: { type: Number, required: true },
+    status: {
+      type: String,
+      required: true,
+      enum: ["Available", "Sold"],
+      default: "Available",
+    },
+    color: {
+      type: String,
+      required: true,
+      enum: [
+        "Black",
+        "Blue",
+        "Brown",
+        "Grey",
+        "Green",
+        "Red",
+        "Silver",
+        "White",
+        "Yellow",
+        "Other",
+      ],
+    },
+    version: { type: Schema.Types.ObjectId, required: true, ref: "Version" },
+    pictures: [
+      {
+        type: Buffer,
+        ref: "Pic",
+      },
+    ],
+    description: { type: String },
+    country: { type: String },
+    email: { type: String },
+    phone: { type: String },
+  },
+  { timestamps: true }
+);
 
 CarSchema.virtual("url").get(function () {
   return `/inventory/car/${this._id}`;
