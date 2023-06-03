10f68230f46ec454b3dd1793b7448604f9dc757e
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-06-03 10:14:00 -0400

    Final form for adding car no longer needs a FormData object due to usage of hiddent inputs

--- public/javascripts/pictureFormFinalHandler.js
+++ public/javascripts/pictureFormFinalHandler.js
@@ -4,7 +4,6 @@ const cloud_name = "dpqwimjsm";
 
 finalForm.addEventListener("submit", async (event) => {
   event.preventDefault();
-  const formData = new FormData(finalForm);
   //Detect pic inputs to modify
   const picInputs = Array.from(document.querySelectorAll("input[type=file]"));
   const loadedPicInputs = [];
@@ -22,6 +21,14 @@ finalForm.addEventListener("submit", async (event) => {
     //Get authorization (signature):
     const authorization = await fetch("/inventory/get-signature").then(
       (response) => response.json()
+    );
+
+    //Get hidden inputs to intervene:
+    const hidden = Array.from(document.querySelectorAll(".picId"));
+    const hiddenTargets = hidden.filter((h) =>
+      loadedPicInputs.some(
+        (l) => l.position == h.getAttribute("name").match(/\d/)[0]
+      )
     );
 
     //Create an array of promises of each pic:
@@ -75,31 +82,23 @@ finalForm.addEventListener("submit", async (event) => {
             const whitelist = txt.split(",");
             processedPics.forEach((p) => {
               if (whitelist.some((w) => w == p.position)) {
-                formData.append(`pic${p.position}`, p.public_id);
+                //formData.append(`pic${p.position}`, p.public_id);
+                hiddenTargets.forEach((h) => {
+                  if (h.getAttribute("class").match(/\d/)[0] == p.position) {
+                    h.value = p.public_id;
+                  }
+                });
               }
             });
           });
       }) //Send processed form to backend:
       .then(() => {
-        fetch("/inventory/add-car/form-submission", {
-          method: "POST",
-          body: formData,
-        })
-          .then((response) => {
-            window.location.href = response.url;
-          })
-          .catch((err) => console.log(err));
+        //Send natural form instead:
+        event.target.submit();
       })
       .catch((err) => console.log(err));
   }
   if (loadedPicInputs.length < 1) {
-    fetch("/inventory/add-car/form-submission", {
-      method: "POST",
-      body: formData,
-    })
-      .then((response) => {
-        window.location.href = response.url;
-      })
-      .catch((err) => console.log(err));
+    event.target.submit();
   }
 });
