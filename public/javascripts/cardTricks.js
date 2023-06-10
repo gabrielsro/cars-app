affaea655e5eb86358f90505ccd214b4872b446b
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-06-10 12:43:55 -0400

    Fixed layout shifts and misalignments on desktop screens

--- public/javascripts/cardTricks.js
+++ public/javascripts/cardTricks.js
@@ -7,13 +7,9 @@ listCards.forEach((c) => {
   let title = c.querySelector(".title p");
   c.addEventListener("mouseover", () => {
     if (subText.scrollWidth > container.offsetWidth) {
-      let finalSubText = `${subText.innerText} | ${subText.innerText} | ${subText.innerText} | ${subText.innerText} | ${subText.innerText} |`;
-      subText.innerText = finalSubText;
       subText.classList.add("autoScrollOnce");
     }
     if (footer.scrollWidth > container.offsetWidth) {
-      const newText = `${footer.innerText} | ${footer.innerText} | ${footer.innerText} | ${footer.innerText} | ${footer.innerText} | ${footer.innerText}`;
-      footer.innerText = newText;
       footer.classList.add("autoScrollOnce");
     }
     if (title.scrollWidth > container.offsetWidth) {
