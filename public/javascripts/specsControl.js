8354edd72be965757a1e99d18da964ba2b967ba3
Gabriel Rodriguez <gabrielsro182@gmail.com>
2023-06-12 16:04:59 -0400

    App UI optimized for mobile viewports. Pending index page optimization

--- public/javascripts/specsControl.js
+++ public/javascripts/specsControl.js
@@ -3,26 +3,28 @@ const hide = document.getElementById("hideSpecs");
 const specs = document.getElementById("specs");
 const mainImage = document.getElementById("main-img");
 
-hide.addEventListener("click", hideSpecs);
-show.addEventListener("click", showSpecs);
-mainImage.addEventListener("click", () => {
-  if (hide.classList.contains("invisible")) {
-    showSpecs();
-    return;
-  }
-  if (!hide.classList.contains("invisible")) {
-    hideSpecs();
-  }
-});
+if (window.innerWidth > 945) {
+  hide.addEventListener("click", hideSpecs);
+  show.addEventListener("click", showSpecs);
+  mainImage.addEventListener("click", () => {
+    if (hide.classList.contains("invisible")) {
+      showSpecs();
+      return;
+    }
+    if (!hide.classList.contains("invisible")) {
+      hideSpecs();
+    }
+  });
 
-function showSpecs() {
-  hide.classList.remove("invisible");
-  show.classList.add("invisible");
-  specs.classList.remove("invisible");
-}
+  function showSpecs() {
+    hide.classList.remove("invisible");
+    show.classList.add("invisible");
+    specs.classList.remove("invisible");
+  }
 
-function hideSpecs() {
-  hide.classList.add("invisible");
-  show.classList.remove("invisible");
-  specs.classList.add("invisible");
+  function hideSpecs() {
+    hide.classList.add("invisible");
+    show.classList.remove("invisible");
+    specs.classList.add("invisible");
+  }
 }
